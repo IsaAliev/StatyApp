b02659b70706a339d79697c0e7c178cf61a8664f
Isa Aliev <isaaliev12@gmail.com>
2019-03-12 14:04:48 +0300

    Initial Commit

---
+++ StatyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatyApp
+//
+//  Created by Isa Aliev on 12.03.2019.
+//  Copyright © 2019 IsaAliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
